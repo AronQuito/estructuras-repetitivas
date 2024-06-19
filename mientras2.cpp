185b9bbc6d6403556bd6a7b9a291531b508a30f8
AronQuito <acachicatariq@unjbg.edu.pe>
2024-06-19 08:33:58 -0500

    Actualizar ejercicio mientras 2: Agregar mensajes al usuario y un pause

--- mientras2.cpp
+++ mientras2.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 int main(){
     int n=1;
+    cout<<"Programa 'parar cuando escriba 0' :"<<endl;
     while (n!=0){
-        cin>>n;
+        cout<<"Escriba un valor: "; cin>>n;
     }
+    cout<<"El programa se ha detenido"<<endl;
+    system("cls");
     return 0;
 }
