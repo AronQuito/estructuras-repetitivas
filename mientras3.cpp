bf00600332da9f4c44200f27aadaebe8c43e4158
AronQuito <acachicatariq@unjbg.edu.pe>
2024-06-20 18:45:55 -0500

    Actualiza mientras 3 de repetitivas: Mejorar interfaz del usuario con mensajes

--- mientras3.cpp
+++ mientras3.cpp
@@ -3,15 +3,16 @@ using namespace std;
 int main(){
     int max,min,n,x=0;
     bool v=1;
-    cout<<"Valor max, valor min: "; cin>>max>>min;
+    cout<<"Identificador de numeros en intervalos!"<<endl<<endl;
+    cout<<"Introduce los intervalos (Valor max y el valor min) : "; cin>>max>>min;
     while(v==1){
-        cin>>n;
+        cout<<"Introduce un valor: "; cin>>n;
         if(n<=max && n>=min){
             x++;
         }else{
             v=0;
         }
     }
-    cout<<x;
+    cout<<"Se encontro "<<x<<" valores correctos introducidos. ";
     return 0;
 }
